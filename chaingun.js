fcc2c4de38df0b802964cc21bd22e7cf3cba6529
justinvdm <justinvdm.0.1.0@gmail.com>
2014-07-26 14:26:46 +0200

    implement the basics

--- chaingun.js
+++ chaingun.js
@@ -1,5 +1,41 @@
 (function() {
-  function chaingun() {}
+  function chaingun(obj) {
+    function _chained_(v) {
+      function _chain_() { return curr; }
+
+      var curr = v;
+      if (typeof obj == 'function') curr = obj.apply(this, arguments);
+
+      return extend(_chain_, obj, function(fn) {
+        if (typeof fn != 'function') return;
+
+        return function() {
+          Array.prototype.unshift.call(arguments, curr);
+          curr = fn.apply(this, arguments);
+          return this;
+        };
+      });
+    }
+
+    return extend(_chained_, obj);
+  }
+
+
+  function extend(target, source, fn) {
+    var result;
+    fn = fn || identity;
+
+    for (var k in source) {
+      if (!source.hasOwnProperty(k)) return;
+      result = fn.call(this, source[k]);
+      if (typeof result != 'undefined') target[k] = result;
+    }
+
+    return target;
+  }
+
+
+  function identity(v) { return v; }
 
   if (typeof module != 'undefined')
     module.exports = chaingun;
--- chaingun.test.js
+++ chaingun.test.js
@@ -1,2 +1,52 @@
+var assert = require('assert');
+var chaingun = require('./chaingun');
+
+
 describe("chaingun", function() {
+  it("should copy across the given object's properties", function() {
+    var thing = {
+      foo: 2,
+      bar: 3,
+      baz: {},
+      quux: function() {}
+    };
+
+    var chain = chaingun(thing);
+    assert.strictEqual(chain.foo, thing.foo);
+    assert.strictEqual(chain.bar, thing.bar);
+    assert.strictEqual(chain.baz, thing.baz);
+    assert.strictEqual(chain.quux, thing.quux);
+  });
+
+  it("should not copy across non-function properties when chaining", function() {
+    assert(!('foo' in chaingun({foo: 2})()));
+  });
+
+  it("should support chaining of the object's function properties", function() {
+    var chain = chaingun({
+      add: function(a, b) { return a + b; },
+      multiply: function(a, b) { return a * b; }
+    });
+
+    var result = chain(2)
+      .add(3)
+      .multiply(5)
+      .add(5)
+      .multiply(2)
+      ();
+
+    assert.equal(result, 60);
+  });
+
+  it("should start chaining with an invoke if the object is a function", function() {
+    function thing(a, b) { return a + b; }
+    thing.multiply = function(a, b) { return a * b; };
+
+    var result = chaingun(thing)
+      (2, 3)
+      .multiply(4)
+      ();
+
+    assert.equal(result, 20);
+  });
 });
